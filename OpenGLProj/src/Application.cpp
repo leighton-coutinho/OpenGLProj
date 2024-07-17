96bb31aceed0c3be05081dc247c6668411dfac81
leighton-coutinho <ljlcoutinho@gmail.com>
2024-07-17 12:10:23 -0400

    abstracted out buffer layout and vertex array

--- OpenGLProj/src/Application.cpp
+++ OpenGLProj/src/Application.cpp
@@ -8,6 +8,7 @@
 #include "Renderer.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "VertexArray.h"
 
 struct ShaderProgramSource
 {
@@ -134,11 +135,13 @@ int main(void)
         GLCall(glGenVertexArrays(1, &vao));
         GLCall(glBindVertexArray(vao));
 
-
+        VertexArray va;
         VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); // binds buffer to vao
+     
         // attribute: 0, size: 2 for 2d points, GL_FLOATS, stride 8 due to float size, pointer starts at 0
 
         IndexBuffer ib(indices, 6);
@@ -169,7 +172,7 @@ int main(void)
             GLCall(glUseProgram(shader));
             GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
-            GLCall(glBindVertexArray(vao));
+            va.Bind();
             ib.Bind();
 
             GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
---
+++ OpenGLProj/src/VertexArray.cpp
@@ -0,0 +1,38 @@
+#include "VertexArray.h"
+#include "Renderer.h"
+
+VertexArray::VertexArray()
+{
+	GLCall(glGenVertexArrays(1, &m_RendererID));	
+}
+
+
+VertexArray::~VertexArray()
+{
+	GLCall(glDeleteVertexArrays(1, &m_RendererID));
+}
+
+void VertexArray::AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout)
+{
+	Bind();
+	vb.Bind();
+	const auto& elements = layout.GetElements();
+	unsigned int offset = 0;
+	for (unsigned int i = 0; i < elements.size(); i++)
+	{
+		const auto& element = elements[i];
+		glEnableVertexAttribArray(i); // attribute: 0, size: 2 for 2d points, GL_FLOATS, stride 8 due to float size, pointer starts at 0
+		glVertexAttribPointer(i, element.count, element.type, element.normalized, layout.GetStride(),(const void*) offset); // binds buffer to vao
+		offset += element.count * VertexBufferElement::GetSizeOfType(element.type);
+	}
+	
+}
+
+void VertexArray::Bind() const
+{
+	GLCall(glBindVertexArray(m_RendererID));
+}
+void VertexArray::Unbind() const
+{
+	GLCall(glBindVertexArray(0));
+}
